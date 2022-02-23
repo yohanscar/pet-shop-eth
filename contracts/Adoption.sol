8292e175169b48979e8f5cf59f37540545f10b87
Wagner Yohan <wyohan@gmail.com>
2022-02-22 22:11:44 -0300

    first compiled contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.5.0;
 contract Adoption {
     address[16] public adopters;
 
-    function adopt(uint256 petId) public returns (uint256) {
+    function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
 
         adopters[petId] = msg.sender;
         return petId;
+    }
+
+    function getAdopters() public view returns(address[16] memory){
+        return adopters;
     }
 }
